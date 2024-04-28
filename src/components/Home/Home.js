6592de25ee1f8f50d9f0fa7e4b525191b2c71ed5
rahul <rahulshanker52@gmail.com>
2024-04-28 21:07:15 +0530

    removed unused components

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Row, Col, Button } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import homeLogo from "../../Assets/home-main.svg";
 import Particle from "../Particle";
 import Type from "./Type";
