99499188779da079760bb2e7f64bbbe4ffad7a64
rahul <rahulshanker52@gmail.com>
2024-04-29 12:25:50 +0530

    resume update

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,36 +1,34 @@
 import React from "react";
 import { Container, Row, Col, Button } from "react-bootstrap";
+import { FaEye } from "react-icons/fa";
+import { IoMdDownload } from "react-icons/io";
 
 function Resume() {
   return (
     <Container className="home-content">
-      <Row className="align-items-center">
-        <Col md={5}>
-          <embed
-            src="/Resume.pdf#toolbar=0&navpanes=0&scrollbar=0"
-            width={400}
-            height={500}
-            type="application/pdf"
-            frameBorder="0"
-            scrolling="auto"
-          ></embed>
-        </Col>
-        <Col mt="100">
-          <a href="/Resume.pdf" target="blank">
-            <Button
-              variant="outline-light"
-              style={{ "margin-right": "50px", width: "100px" }}
-            >
-              view
-            </Button>
-          </a>
-          <a href="/Resume.pdf" download={"Resume.pdf"}>
-            <Button variant="outline-light" style={{ width: "100px" }}>
-              Download
-            </Button>
-          </a>
-        </Col>
-      </Row>
+      <div
+        style={{
+          zIndex: -1,
+          backgroundImage: "url('/Resume.jpg')",
+          height: "1650px",
+          filter: "blur(0)",
+        }}
+      >
+        <Row>
+          <Col style={{ textAlign: "right", margin: "2px" }}>
+            <a href="/Resume.pdf" target="blank" style={{ margin: "2px" }}>
+              <Button variant="outline-dark">
+                View <FaEye />
+              </Button>
+            </a>
+            <a href="/Resume.pdf" download={"Resume.pdf"}>
+              <Button variant="outline-dark">
+                Download <IoMdDownload />
+              </Button>
+            </a>
+          </Col>
+        </Row>
+      </div>
     </Container>
   );
 }
